6c7acbe5ceb19884bd1fadeb7047ebccc2da50d4
Yun Ha <rice86@naver.com>
2020-10-17 14:11:55 +0900

    Initial Commit

---
+++ wmp-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wmp-sample
+//
+//  Created by Yun Ha on 2020/10/17.
+//  Copyright © 2020 Yun Ha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
